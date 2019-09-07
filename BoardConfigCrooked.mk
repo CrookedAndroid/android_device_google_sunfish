a03dddfb05acc059e9559cccca8a1f463be0fd69
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-07 11:13:09 -0700

    sunfish: Build a disabled vbmeta image
    Squashed with:
    Author: Nolen Johnson <johnsonnolen@gmail.com>
    Date:   Sun May 9 16:04:34 2021 -0400
    
        sunfish: Simplify AVB flag logic
    
        * AVB's `--flags 3` argument builds a disabled VBMeta with
          disabled hashtree, so no need to set both flags individually.
    
        Change-Id: Ibe9ee322a5af58f13e76d53e18dee49451fe8838
    
    Change-Id: Ie8f1e0929b037b35b5e45c2e31be9253fa0bb097

--- BoardConfigCrooked.mk
+++ BoardConfigCrooked.mk
@@ -14,3 +14,6 @@ KERNEL_SUPPORTS_LLVM_TOOLS := true
 TARGET_KERNEL_CONFIG := sunfish_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/msm-4.14
 TARGET_NEEDS_DTBOIMAGE := true
+
+# Verified Boot
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
