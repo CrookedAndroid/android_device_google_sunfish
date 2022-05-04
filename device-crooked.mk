6bbc4280a005e1b9e0cc724f9f7e2895d3c346ec
Michael Bestas <mkbestas@gmail.com>
2022-05-04 20:11:46 +0300

    Convert HbmSVManager overlays to RRO
    Auto generated RRO is not working here because we don't actually have
    access to vendor/google_devices/*/proprietary/display/HbmSVManager
    
    Change-Id: I54d885c233151498372e1b510284d6a297ac3e94

--- device-crooked.mk
+++ device-crooked.mk
@@ -21,6 +21,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
+# HBM
+PRODUCT_PACKAGES += \
+    HbmSVManagerOverlay
+
 # RCS
 PRODUCT_PACKAGES += \
     PresencePolling \
